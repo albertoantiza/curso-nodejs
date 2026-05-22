f29376871b3e1eba04cd65af247482ce40f326df
/elchopo7 <elchopo7@gmail.com>
2026-05-22 19:26:04 +0200

    clase 1

---
+++ clase1/http.js
@@ -0,0 +1,17 @@
+const http = require('node:http')
+const { findAvailablePort } = require('./')
+
+console.log(process.env)
+
+const desiredPort = process.env.PORT ?? 3000
+
+const server = http.createServer((req, res) => {
+    console.log('request received')
+    res.end('Hola mundo')
+})
+
+findAvailablePort(desiredPort).then(port => {
+    server.listen(port => {
+        console.log(`server listenning in port http://localhost:${port}`)
+    })
+})
