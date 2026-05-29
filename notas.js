68c4e5b3640c94670899e3df0257ac655ddf860e
/elchopo7 <elchopo7@gmail.com>
2026-05-29 20:03:21 +0200

    Update notas.js

--- notas.js
+++ notas.js
@@ -20,4 +20,9 @@ const promise = new Promise((resolve, reject) => {
   }
 })
 
-promise.then((response) => console.log(response))
+try {
+  const response = await promise
+  console.log(response)
+} catch (err) {
+  console.log(err)
+}
