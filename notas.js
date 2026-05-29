e48b2df6a81b6badd649688679fffd7bc6aabd2f
/elchopo7 <elchopo7@gmail.com>
2026-05-29 19:54:48 +0200

    Update notas.js

--- notas.js
+++ notas.js
@@ -8,4 +8,16 @@ async function fetchData () {
 }
 
 fetchData()
+
 // Ejercicio 2: Promises and async/await
+
+const promise = new Promise((resolve, reject) => {
+  const success = Math.random() > 0.5
+  if (success) {
+    resolve('Operation resolved')
+  } else {
+    reject(new Error('Operation rejected'))
+  }
+})
+
+promise.then((response) => console.log(response))
