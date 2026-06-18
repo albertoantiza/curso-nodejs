ac51f6660d2f5ed6c2ff303025201971707b7c7a
/elchopo7 <elchopo7@gmail.com>
2026-06-18 12:35:40 +0200

    Update http.js

--- clase1/http.js
+++ clase1/http.js
@@ -3,15 +3,15 @@ const { findAvailablePort } = require('./clase/free-port.js')
 
 console.log(process.env)
 
-const desiredPort = process.env.PORT ?? 3000
+const desiredPort = process.env.PORT ?? 3400
 
 const server = http.createServer((req, res) => {
-    console.log('request received')
-    res.end('Hola mundo')
+  console.log('request received: ', req.url)
+  res.end('Hola mundo')
 })
 
 findAvailablePort(desiredPort).then(port => {
-    server.listen(port => {
-        console.log(`server listenning in port http://localhost:${port}`)
-    })
+  server.listen(port => {
+    console.log(`server listenning in port http://localhost:${port}`)
+  })
 })
