c55bf5da640525df14b0472ff8cbd1cb17f86297
/elchopo7 <elchopo7@gmail.com>
2026-06-19 19:26:25 +0200

    Update routing.js

--- clase1/routing.js
+++ clase1/routing.js
@@ -6,3 +6,6 @@ const processRequest = (req, res) => {
 
 const server = http.createServer(processRequest)
 
+server.listen(1234, () => {
+  console.log('server listenning on port http://localhost:1234')
+})
