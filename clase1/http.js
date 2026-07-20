4502e7ff7c36e7293a55f9eede48f8a762f12b6e
/elchopo7 <elchopo7@gmail.com>
2026-07-20 18:59:35 +0200

    Create http.js

---
+++ clase1/http.js
@@ -0,0 +1,22 @@
+const http = require('node:http')
+const { findAvailablePort } = require('./clase/free-port.js')
+
+console.log(process.env)
+
+const desiredPort = process.env.PORT ?? 3400
+
+const processRequest = (req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.end('Bienvenido a mi página de inicio')
+  } else if (req.url === '/contacto')
+}
+
+const server = http.createServer(processRequest)
+
+findAvailablePort(desiredPort).then(port => {
+  server.listen(port => {
+    console.log(`server listenning in port http://localhost:${port}`)
+  })
+})
